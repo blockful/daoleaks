fb126a32131dd6c01ad075051a6494c9568f4dff
Alexandro T. Netto <alex.t.netto@gmail.com>
2025-08-15 23:30:35 -0400

    feat: tutorial working on vite

--- apps/front/src/App.tsx
+++ apps/front/src/App.tsx
@@ -1,34 +1,67 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { UltraHonkBackend } from '@aztec/bb.js'
+import { Noir } from '@noir-lang/noir_js'
+// @ts-ignore
+import circuit from '../../circuits/target/circuit.json'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [age, setAge] = useState('')
+  const [logs, setLogs] = useState<string[]>([])
+  const [proof, setProof] = useState<string>('')
+
+  const addLog = (content: string) => {
+    setLogs(prev => [...prev, content])
+  }
+
+  const handleSubmit = async () => {
+    try {
+      const noir = new Noir(circuit as any)
+      const backend = new UltraHonkBackend(circuit.bytecode)
+      
+      addLog("Generating witness... ⏳")
+      const { witness } = await noir.execute({ age: parseInt(age) })
+      addLog("Generated witness... ✅")
+      
+      addLog("Generating proof... ⏳")
+      const proofResult = await backend.generateProof(witness)
+      addLog("Generated proof... ✅")
+      setProof(String(proofResult.proof))
+      
+      addLog('Verifying proof... ⌛')
+      const isValid = await backend.verifyProof(proofResult)
+      addLog(`Proof is ${isValid ? "valid" : "invalid"}... ✅`)
+    } catch (error) {
+      console.error("Error", error)
+      addLog("Oh 💔")
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <h1>Noir app</h1>
+      <div className="input-area">
+        <input
+          type="number"
+          placeholder="Enter age"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <button onClick={handleSubmit}>Submit Age</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="outer">
+        <div className="inner">
+          <h2>Logs</h2>
+          {logs.map((log, index) => (
+            <div key={index}>{log}</div>
+          ))}
+        </div>
+        <div className="inner">
+          <h2>Proof</h2>
+          {proof && <div>{proof}</div>}
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
