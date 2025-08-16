24cb794e89d098709554c5017aee60bc3856abb8
Alexandro T. Netto <alex.t.netto@gmail.com>
2025-08-16 17:45:35 -0400

    feat: tailwind v4 on vite

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   optimizeDeps: {
     esbuildOptions: { target: "esnext" },
     exclude: ['@noir-lang/noirc_abi', '@noir-lang/acvm_js']
