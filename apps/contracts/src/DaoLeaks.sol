5b23346be337ddcdc20cff691de6d46b4bafc6c3
Alexandro T. Netto <alex.t.netto@gmail.com>
2025-08-16 09:08:30 -0400

    feat: getters and compiling

--- apps/contracts/src/DaoLeaks.sol
+++ apps/contracts/src/DaoLeaks.sol
@@ -11,19 +11,27 @@ contract DaoLeaks {
     }
     Message[] public messages;
 
-    
+    HonkVerifier public verifier;
+
+    // Constructor
+    constructor(address _verifier) {
+        verifier = HonkVerifier(_verifier);
+    }
+
     // Events
 
     // When someone posts a message: message, voting power, timestamp
     event MessagePosted(string message, uint256 votingPowerLevel, uint256 timestamp);
 
+    // Functions
 
-    function postMessage(bytes calldata proof, string memory message, uint256 votingPowerLevel) public {
+    // When someone posts a message: proof, public inputs, message, voting power level
+    function postMessage(bytes calldata proof, bytes32[] memory publicInputs, string memory message, uint256 votingPowerLevel) public {
         // Get the public inputs
-        bytes32[] memory publicInputs = new bytes32[](96);
-        // Verify the proof
-        HonkVerifier.verify(proof, publicInputs);
+        // bytes32[] memory publicInputs = new bytes32[](96);
 
+        // Verify the proof
+        verifier.verify(proof, publicInputs);
 
         // Add the message to the array
         messages.push(Message({message: message, votingPowerLevel: votingPowerLevel, timestamp: block.timestamp}));
@@ -32,9 +40,38 @@ contract DaoLeaks {
         emit MessagePosted(message, votingPowerLevel, block.timestamp);
     }
 
-    function getMessages() public view returns (Message[] memory) {
-        return messages;
+    // Get all messages paginated
+    function getMessages(uint256 page, uint256 pageSize) public view returns (Message[] memory) {
+        // Get the start and end index
+        uint256 startIndex = page * pageSize;
+        uint256 endIndex = startIndex + pageSize;
+        
+        // Make sure endIndex doesn't exceed array length
+        if (endIndex > messages.length) {
+            endIndex = messages.length;
+        }
+        
+        // Calculate actual size of the return array
+        uint256 resultSize = endIndex - startIndex;
+        
+        // Create a new array in memory with the correct size
+        Message[] memory result = new Message[](resultSize);
+        
+        // Copy elements to the new array
+        for (uint256 i = 0; i < resultSize; i++) {
+            result[i] = messages[startIndex + i];
+        }
+        
+        return result;
     }
-    
 
+    // Get the total number of messages
+    function getTotalMessages() public view returns (uint256) {
+        return messages.length;
+    }
+
+    // Get the total number of pages
+    function getTotalPages(uint256 pageSize) public view returns (uint256) {
+        return messages.length / pageSize;
+    }
 }
